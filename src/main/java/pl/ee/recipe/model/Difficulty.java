76b3e9ad3c8efeb25146e066c5c676054901cd16
Hubert Dryja <hubertdryja@gmail.com>
2018-10-29 12:03:55 +0100

    add models

---
+++ src/main/java/pl/ee/recipe/model/Difficulty.java
@@ -0,0 +1,5 @@
+package pl.ee.recipe.model;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
---
+++ src/main/java/pl/ee/recipe/model/Note.java
@@ -0,0 +1,21 @@
+package pl.ee.recipe.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNote;
+}
