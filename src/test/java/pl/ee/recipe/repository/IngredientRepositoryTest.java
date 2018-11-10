5eaf908829878c022a1b4d9da188c065435c4856
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-11-10 22:27:52 +0100

    Add tests for UnitOfMeasure - close #4

---
+++ src/test/java/pl/ee/recipe/repository/IngredientRepositoryTest.java
@@ -0,0 +1,35 @@
+package pl.ee.recipe.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class IngredientRepositoryTest {
+
+    @Autowired
+    IngredientRepository ingredientRepository;
+
+    @Test
+    public void create() {
+
+    }
+
+    @Test
+    public void read() {
+
+    }
+
+    @Test
+    public void update() {
+
+    }
+
+    @Test
+    public void delete() {
+
+    }
+}
---
+++ src/test/java/pl/ee/recipe/repository/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,73 @@
+package pl.ee.recipe.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.ee.recipe.model.UnitOfMeasure;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    public void create() {
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setId(3L);
+        unitOfMeasure.setDescription("pint");
+
+        UnitOfMeasure unitOfMeasureSaved =  unitOfMeasureRepository.save(unitOfMeasure);
+        Iterable<UnitOfMeasure> unitOfMeasures = unitOfMeasureRepository.findAll();
+
+        assertEquals(Long.valueOf(3L), unitOfMeasureSaved.getId());
+        assertEquals("pint", unitOfMeasureSaved.getDescription());
+        assertEquals(3, ((Collection<?>) unitOfMeasures).size());
+        assertTrue(unitOfMeasureRepository.existsById(3L));
+    }
+
+    @Test
+    public void read() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findById(1L);
+        UnitOfMeasure unitOfMeasure = unitOfMeasureOptional.orElseThrow(RuntimeException::new);
+
+        assertEquals(Long.valueOf(1L), unitOfMeasure.getId());
+        assertEquals("cup", unitOfMeasure.getDescription());
+    }
+
+    @Test
+    public void update() {
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setId(1L);
+        unitOfMeasure.setDescription("pint");
+
+        UnitOfMeasure unitOfMeasureUpdated = unitOfMeasureRepository.save(unitOfMeasure);
+        Iterable<UnitOfMeasure> unitOfMeasures = unitOfMeasureRepository.findAll();
+
+        assertEquals(Long.valueOf(1L), unitOfMeasureUpdated.getId());
+        assertEquals("pint", unitOfMeasureUpdated.getDescription());
+        assertEquals(2, ((Collection<?>) unitOfMeasures).size());
+        assertTrue(unitOfMeasureRepository.existsById(1L));
+    }
+
+    @Test
+    public void delete() {
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setId(1L);
+        unitOfMeasure.setDescription("cup");
+
+        unitOfMeasureRepository.delete(unitOfMeasure);
+        Iterable<UnitOfMeasure> unitOfMeasures = unitOfMeasureRepository.findAll();
+
+        assertEquals(1, ((Collection<?>) unitOfMeasures).size());
+        assertFalse(unitOfMeasureRepository.existsById(1L));
+    }
+}
