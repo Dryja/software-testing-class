ef59cce5e64dc40d536c02e95b59b8dd2204a71d
berskik <berskik@ee.pw.edu.pl>
2018-11-12 21:41:24 +0100

    Added tests for Recipe and Ingredient models

--- src/test/java/pl/ee/recipe/repository/IngredientRepositoryTest.java
+++ src/test/java/pl/ee/recipe/repository/IngredientRepositoryTest.java
@@ -1,10 +1,19 @@
 package pl.ee.recipe.repository;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import pl.ee.recipe.model.Ingredient;
+import pl.ee.recipe.model.Note;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -14,22 +23,68 @@ public class IngredientRepositoryTest {
     IngredientRepository ingredientRepository;
 
     @Test
-    public void create() {
-
+    public void create() { 	
+    	BigDecimal amount = new BigDecimal(0.35);
+        Ingredient ingr = new Ingredient();
+        ingr.setId(1L);
+        ingr.setDescription("Dorodny buraczek");
+        ingr.setAmount(amount); //Przepis głównie oparty na dorodnych buraczkach
+        
+        Ingredient savedIngr = ingredientRepository.save(ingr);
+        assertTrue(ingredientRepository.existsById(1L));
+        
+        assertEquals(Long.valueOf(1L), savedIngr.getId());
+        assertEquals("Dorodny buraczek", savedIngr.getDescription());
+        assertEquals(amount, savedIngr.getAmount());     
     }
 
     @Test
     public void read() {
-
+    	BigDecimal amount = new BigDecimal(0.35);
+    	Ingredient ingr = new Ingredient();
+        ingr.setId(5L);
+        ingr.setDescription("Dorodny buraczek");
+        ingr.setAmount(amount); //Przepis głównie oparty na dorodnych buraczkach
+        
+        Ingredient savedIngr = ingredientRepository.save(ingr);
+        
+        Optional<Ingredient> singleIngrOpt = ingredientRepository.findById(savedIngr.getId());
+        Ingredient singleIngr = singleIngrOpt.orElseThrow(RuntimeException::new);
+        assertEquals(singleIngr.getDescription(), "Dorodny buraczek");
+        assertEquals(singleIngr.getAmount(), amount);
+        assertEquals(singleIngr.getId(), Long.valueOf(4L));
     }
 
     @Test
     public void update() {
+    	BigDecimal amount = new BigDecimal(0.35);
+    	BigDecimal amountUpdated = new BigDecimal(0.45);
+    	Ingredient ingr = new Ingredient();
+        ingr.setId(3L);
+        ingr.setDescription("Dorodny buraczek");
+        ingr.setAmount(amount);
+        
+        Ingredient savedIngr = ingredientRepository.save(ingr);
+        
+        savedIngr.setDescription("Niedorodny Buraczek");
+        savedIngr.setAmount(amountUpdated);
+        ingredientRepository.save(savedIngr);
 
+        Optional<Ingredient> singleIngrOpt = ingredientRepository.findById(savedIngr.getId());
+        Ingredient singleIngr = singleIngrOpt.orElseThrow(RuntimeException::new);
+        assertEquals(singleIngr.getDescription(), "Niedorodny Buraczek");
+        assertEquals(singleIngr.getAmount(), amountUpdated);
     }
 
     @Test
     public void delete() {
+    	BigDecimal amount = new BigDecimal(0.35);
+    	Ingredient ingr = new Ingredient();
+        ingredientRepository.save(ingr);
+        
+        assertEquals(1, ingredientRepository.count());
 
+        ingredientRepository.delete(ingr);
+        assertEquals(0, ingredientRepository.count());
     }
 }
--- src/test/java/pl/ee/recipe/repository/RecipeRepositoryTest.java
+++ src/test/java/pl/ee/recipe/repository/RecipeRepositoryTest.java
@@ -1,10 +1,18 @@
 package pl.ee.recipe.repository;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import pl.ee.recipe.model.Recipe;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -14,22 +22,76 @@ public class RecipeRepositoryTest {
     RecipeRepository recipeRepository;
 
     @Test
-    public void create() {
-
+    public void create() { 	
+    	Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipe.setDescription("Dobry przepisik");
+        recipe.setPrepTime(1);
+        recipe.setCookTime(1);
+        recipe.setServings(1);
+        
+        Recipe savedRecipe = recipeRepository.save(recipe);
+        assertTrue(recipeRepository.existsById(1L));
+        
+        assertEquals(Long.valueOf(1L), savedRecipe.getId());
+        assertEquals("Dobry przepisik", savedRecipe.getDescription());
+        assertEquals(new Integer(1), savedRecipe.getPrepTime());   
+        assertEquals(new Integer(1), savedRecipe.getCookTime());   
+        assertEquals(new Integer(1), savedRecipe.getServings());   
     }
 
     @Test
     public void read() {
-
+    	Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipe.setDescription("Dobry przepisik");
+        recipe.setPrepTime(1);
+        recipe.setCookTime(1);
+        recipe.setServings(1);
+        
+        Recipe savedRecipe = recipeRepository.save(recipe);
+        
+        Optional<Recipe> singleRecipeOpt = recipeRepository.findById(savedRecipe.getId());
+        Recipe singleRecipe = singleRecipeOpt.orElseThrow(RuntimeException::new);
+        assertEquals("Dobry przepisik", singleRecipe.getDescription());
+        assertEquals(new Integer(1), singleRecipe.getPrepTime());   
+        assertEquals(new Integer(1), singleRecipe.getCookTime());   
+        assertEquals(new Integer(1), singleRecipe.getServings()); 
     }
 
     @Test
     public void update() {
+    	Recipe recipe = new Recipe();
+        recipe.setId(3L);
+        recipe.setDescription("Dobry przepisik");
+        recipe.setPrepTime(1);
+        recipe.setCookTime(1);
+        recipe.setServings(1);
+  
+        Recipe savedRecipe = recipeRepository.save(recipe);
+        savedRecipe.setPrepTime(2);
+        savedRecipe.setCookTime(2);
+        savedRecipe.setServings(2);
+        savedRecipe.setDescription("Słaby przepisik");
+        recipeRepository.save(savedRecipe);
 
+        Optional<Recipe> singleRecipeOpt = recipeRepository.findById(savedRecipe.getId());
+        Recipe singleRecipe = singleRecipeOpt.orElseThrow(RuntimeException::new);
+        assertEquals("Słaby przepisik", singleRecipe.getDescription());
+        assertEquals(new Integer(2), singleRecipe.getPrepTime());   
+        assertEquals(new Integer(2), singleRecipe.getCookTime());   
+        assertEquals(new Integer(2), singleRecipe.getServings()); 
     }
 
     @Test
     public void delete() {
+    	BigDecimal amount = new BigDecimal(0.35);
+    	Recipe recipe = new Recipe();
+        recipeRepository.save(recipe);
+        
+        assertEquals(1, recipeRepository.count());
 
+        recipeRepository.delete(recipe);
+        assertEquals(0, recipeRepository.count());
     }
 }
